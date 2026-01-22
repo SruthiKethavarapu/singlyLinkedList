d5ae5eadadc893e32803ae52db9c4fb090ae188d
SruthiKethavarapu <kethavarapuharshitha@gmail.com>
2026-01-22 07:48:55 +0530

    Add files via upload

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define LISTEMPTY printf("\nList is empty\n");
 #define ITEMNOTFOUND printf("\nItem not found\n");
 #define ITEMDELETED printf("Item deleted successfully.\n");
+#define ITEMUPDATED printf("Item updated successfully.\n");
 #define DATAFILE "itemRecords.dat"
 
 struct item 
@@ -14,22 +16,15 @@ struct item
 	char itemName[30];
 	float itemPrice;
 };
+
 struct node
 {
 	struct item data;
 	struct node *next; 
 };
 
-void insertItem();
-void displayItems();
-int deleteItem(struct node *itemToDelete);
-int updateItem(struct node *itemToUpdate);
-void sortListByPrice();
-void sortListByNames();
-struct node* getItemByID();
-struct node* searchByID(char searchID[10]);
-
 struct node *head = NULL;
+struct node *copyHead = NULL;
 struct node* createNode(char itemID[], char itemName[], float itemPrice)
 {
 	struct node *newNode = (struct node *)malloc(sizeof(struct node));
@@ -48,7 +43,7 @@ void insertItem()
     scanf("%s", itemID);
     printf("Item Name: ");
     scanf("%s", itemName);
-    printf("Item Price: ");
+	printf("Item Price: ");
     scanf("%f", &itemPrice);
 
 	struct node *newNode = createNode(itemID, itemName, itemPrice);
@@ -65,22 +60,43 @@ void insertItem()
 	temporaryNode->next = newNode;
 }
 
-void displayItems()
+void displayItems(struct node *start)
 {
-	struct node *temporaryNode = head;
-	if (temporaryNode == NULL)
+	if (start == NULL)
 	{
 		LISTEMPTY
 		return;
 	}
 	printf("\n--- Item List ---\n");
-	while (temporaryNode!= NULL)
+	while (start!= NULL)
 	{
-		printf("Item ID: %s\n", temporaryNode->data.itemID);
-        printf("Item Name: %s\n", temporaryNode->data.itemName);
-        printf("Item Price: %.2f\n\n", temporaryNode->data.itemPrice);
-        temporaryNode = temporaryNode->next;
+		printf("Item ID: %s\n", start->data.itemID);
+        printf("Item Name: %s\n", start->data.itemName);
+        printf("Item Price: %.2f\n\n", start->data.itemPrice);
+        start = start->next;
 	}
+}
+
+struct node* searchByID(char *searchID)
+{
+	struct node *temporaryNode = head;
+	while(temporaryNode != NULL)
+	{
+		if(strcmp(temporaryNode->data.itemID, searchID) == 0)
+		{
+			return temporaryNode;
+		}
+		temporaryNode = temporaryNode->next;
+	}
+	return NULL;
+}
+
+struct node* getItemByID()
+{
+    char searchID[10];
+    printf("Enter item ID: ");
+    scanf("%s", searchID);
+    return searchByID(searchID);
 }
 
 int updateItem(struct node *itemToUpdate)
@@ -88,13 +104,11 @@ int updateItem(struct node *itemToUpdate)
 	float newPrice;
 	if (itemToUpdate == NULL)
 	{
-		ITEMNOTFOUND
 		return 0;
 	}
 	printf("Enter new price: ");
 	scanf("%f", &newPrice);
 	itemToUpdate->data.itemPrice = newPrice;
-	printf("Item updated successfully.\n");	
 	return 1;
 }
 
@@ -103,19 +117,60 @@ int deleteItem(struct node *itemToDelete)
 	struct node **item = &head;
 	if (itemToDelete == NULL)
 	{
-		ITEMNOTFOUND
 		return 0;
 	}	
-	item = &head;
-	while((*item) != itemToDelete)
+	while(*item != itemToDelete)
 	{
 		item = &((*item)->next);
 	}
-	*item = (*item)->next;
+	*item = itemToDelete->next;
 	free(itemToDelete);
-	ITEMDELETED
 	return 1;
 }
+
+// int getNodeIndex(struct node *id)
+// {
+// 	struct node *temporaryNode = head;
+// 	int index = 0;
+// 	while(temporaryNode != NULL)
+// 	{
+// 		index++;
+// 		if(id == temporaryNode)
+// 		{
+// 			return index;
+// 		}
+// 		temporaryNode = temporaryNode->next;
+// 	}
+// 	return -1;
+// }
+
+int getNodeIndex(char *id)
+{
+	int index = 0;
+	struct node *temporaryNode = head;
+	while(temporaryNode != NULL)
+	{
+		index++;
+		if(strcmp(temporaryNode->data.itemID, id) == 0)
+		{
+			return index;
+		}
+		temporaryNode = temporaryNode->next;
+	}
+	return -1;
+}
+
+// float getInventoryTotal()
+// {
+// 	float inventoryToatal = 0;
+// 	struct node *temporaryNode = head;
+// 	while(temporaryNode != NULL)
+// 	{
+// 		inventoryToatal += temporaryNode->data.itemQunatity * temporaryNode->data.itemPrice;
+// 		temporaryNode = temporaryNode->next;
+// 	}
+// 	return inventoryToatal;
+// }
 
 void saveList()
 {
@@ -127,7 +182,6 @@ void saveList()
 		fwrite(&temporaryNode->data, sizeof(struct item), 1, fpNode);
 		temporaryNode = temporaryNode->next;
 	}
-	printf("Items saved successfully.\n");
 	fclose(fpNode);
 }
 void loadDataIntoList()
@@ -156,9 +210,46 @@ void loadDataIntoList()
 	fclose(fpRecord);
 }
 
-void sortListByPrice()
+void copyOriginalList()
 {
-	if(head == NULL || head->next == NULL)
+
+    struct node *temp = head;   
+    struct node *last = NULL;   
+
+    copyHead = NULL;
+
+    while (temp != NULL)
+    {
+        struct node *newNode = createNode(temp->data.itemID, temp->data.itemName, temp->data.itemPrice);
+		if (copyHead == NULL)
+        {
+            copyHead = newNode;   
+            last = newNode;       
+        }
+        else
+        {
+            last->next = newNode; 
+            last = newNode;       
+        }
+
+        temp = temp->next;        
+    }
+}
+
+void freeCopyList()
+{
+    struct node *temp;
+    while (copyHead != NULL)
+    {
+        temp = copyHead;
+        copyHead = copyHead->next;
+        free(temp);
+    }
+}
+
+void sortCopyListByPrice()
+{
+	if(copyHead == NULL || copyHead->next == NULL)
 	{
 		return;
 	}
@@ -168,7 +259,7 @@ void sortListByPrice()
 	while(swapped)
 	{
 		swapped = 0;
-		current = head;
+		current = copyHead;
 		while(current->next != NULL)
 		{
 			if(current->data.itemPrice > current->next->data.itemPrice)
@@ -185,90 +276,137 @@ void sortListByPrice()
 
 void sortListByNames()
 {
-	if(head == NULL || head->next == NULL)
+	if(copyHead == NULL || copyHead->next == NULL)
 	{
 		return;
 	}
 	struct node *current;
-	int swapped = 1;
+	struct node *temporaryNode;
 	struct item temp;
-	while(swapped)
+	for(current = copyHead; current->next != NULL; current=current->next)
 	{
-		swapped = 0;
-		current = head;
-		while(current->next != NULL)
+		for(temporaryNode = copyHead; temporaryNode->next != NULL; temporaryNode = temporaryNode->next)
 		{
-			if(strcmp(current->data.itemName, current->next->data.itemName) > 0)
+			if(strcmp(temporaryNode->data.itemName, temporaryNode->next->data.itemName) >0)
 			{
-				temp = current->data;
-				current->data = current->next->data;
-				current->next->data = temp;
-				swapped = 1;
+				temp = temporaryNode->data;
+				temporaryNode->data = temporaryNode->next->data;
+				temporaryNode->next->data = temp;
 			}
-			current = current->next;
 		}
 	}
 }
 
-struct node* getItemByID()
-{
-    char searchID[10];
-    printf("Enter item ID: ");
-    scanf("%s", searchID);
-    return searchByID(searchID);
-}
-
-struct node* searchByID(char *searchID)
+void printHighestPriceNode()
 {
 	struct node *temporaryNode = head;
+	struct node *maxNode = head;
 	while(temporaryNode != NULL)
 	{
-		if(strcmp(temporaryNode->data.itemID, searchID) == 0)
+		if(temporaryNode->data.itemPrice > maxNode->data.itemPrice)
 		{
-			return temporaryNode;
+			maxNode = temporaryNode;
 		}
 		temporaryNode = temporaryNode->next;
 	}
-	return NULL;
+	printf("Highest price item: \n");
+	printf("Item ID: %s\n", maxNode->data.itemID);
+	printf("Item Name: %s\n", maxNode->data.itemName);
+	printf("Item Price: %.2f\n", maxNode->data.itemPrice);
+}
+
+void printLowestPriceNode()
+{
+	struct node *temporaryNode = head;
+	struct node *minNode = head;
+	while(temporaryNode != NULL)
+	{
+		if(temporaryNode->data.itemPrice < minNode->data.itemPrice)
+		{
+			minNode = temporaryNode;
+		}
+		temporaryNode = temporaryNode->next;
+	}
+	printf("Lowest price item: \n");
+	printf("Item ID: %s\n", minNode->data.itemID);
+	printf("Item Name: %s\n", minNode->data.itemName);
+	printf("Item Price: %.2f\n", minNode->data.itemPrice);
+}
+
+int getRecordCount()
+{
+	int recordCounter = 0;
+	struct node *temporaryNode = head;
+	while(temporaryNode != NULL)
+	{		
+		temporaryNode = temporaryNode->next;
+		recordCounter++;
+	}
+	return recordCounter;
 }
 
 int main() 
 {
 	int choice;
-	char searchID[10];
 	struct node *found;
 	loadDataIntoList();
 	while (1)
 	{
-		printf("\n1. Insert item\n2. Display item\n3. Update item\n4. Delete item\n5. Sort item by price\n6. Sort item by name\n7. Save list\n8. Exit\n\n");
+		printf("\n1. Insert item\n2. Display item\n3. Update item\n4. Delete item\n5. Sort item by price\n6. Sort item by name\n7. Print highest price node\n8. Print lowest price item\n9. Exit\n\n");
 		printf("Enter your choice: ");
 		scanf("%d", &choice);
 		switch (choice)
         {
             case 1: 
             	insertItem(); 
+            	saveList();
             	break;
             case 2: 
-            	displayItems(); 
+            	displayItems(head); 
             	break;
             case 3: 
                 found = getItemByID();
-                updateItem(found); 
+                if(updateItem(found)) 
+                {
+                	ITEMUPDATED
+                }
+                else
+                {
+                	ITEMNOTFOUND
+                }
+                saveList();
             	break;
             case 4: 
                 found = getItemByID();
-                deleteItem(found); 
+                if(deleteItem(found))
+                {
+                	ITEMDELETED
+                } 
+                else
+                {
+                	ITEMNOTFOUND
+                }
+                saveList();
             	break;
             case 5:
-            	sortListByPrice();
+            	freeCopyList();
+            	copyOriginalList();
+            	sortCopyListByPrice();
+            	displayItems(copyHead);
             	break;
             case 6:
+            	freeCopyList();
+            	copyOriginalList();
             	sortListByNames();
+            	displayItems(copyHead);
             	break;
             case 7:
-            	saveList();
+            	printHighestPriceNode();
             	break;
-            case 8: 
+            case 8:
+            	printLowestPriceNode();
+            	break;
+            case 9: 
             	printf("Exiting...\n"); 
             	return 0;
             default: 
@@ -276,5 +414,4 @@ int main()
         }
 	}
 	return 0;
-
 }
